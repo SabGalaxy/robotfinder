a80c0463681f043240cb83dce7ca571ad96e4322
Sabarish Hariraj <sabarishraj88@gmail.com>
2018-10-28 18:08:17 -0400

    Updates

---
+++ precache-manifest.72680f2790530ae75c7d5aeed7c2ce9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/robotfinder/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3b72f8030972b878049c",
+    "url": "/robotfinder/static/js/runtime~main.3b72f803.js"
+  },
+  {
+    "revision": "492465e8b5c368167bfb",
+    "url": "/robotfinder/static/js/main.492465e8.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/robotfinder/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "492465e8b5c368167bfb",
+    "url": "/robotfinder/static/css/main.fe73f758.chunk.css"
+  },
+  {
+    "revision": "89b03bc450276cb63bdb95795ae6aa25",
+    "url": "/robotfinder/index.html"
+  }
+];
